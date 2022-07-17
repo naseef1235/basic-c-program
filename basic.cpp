5d821f98fc40f738775bdc082cd82a1e9da49004
naseef1235 <80143146+naseef1235@users.noreply.github.com>
2022-07-17 22:07:48 +0530

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ using namespace std;
+ strct student
+ {
+  int rollno;
+      char name[30]
+		int mark;
+	};
+ int main()
+  { student s;
+    cout<<"Enter Rollno";
+	 cin>> S.rollno;
+	 cout<<"Enter Name";
+	 cin>>"S.name;
+	 cout<<"Enter Mark";
+	 cin>>"S.mark;
+	 Cout <<"Rollno :" <<S.Rollno;
+	 cout <<"\n Name :" <<S.name;
+	 cout <<"\n Mark :" <<S.mark;
+	}
